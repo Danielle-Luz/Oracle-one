f9f5e25929e47e09e5a6276514818febe1c2b38a
Axionauta <daniln2209@gmail.com>
2022-08-13 15:45:10 -0300

    adição de script pra converter letras com acento em letras sem acento

--- Challenges/sprint_1/src/js/scripts.js
+++ Challenges/sprint_1/src/js/scripts.js
@@ -6,6 +6,7 @@ const botoes = document.querySelectorAll(".botoes button");
 const b_copiar = document.getElementById("b-copiar");
 const container_alert = document.querySelector (".container-dialog");
 const container_alert_botao = container_alert.querySelector ("button");
+const acentos = [["à", "á", "ã", "â"], ["é", "è", "ê"], ["í, ì, î"], ["ó", "ò", "õ", "ô"], ["ú", "ù", "û"]];
 const letras = ["a", "e", "i", "o", "u"];
 const correspondencia = ["ai", "enter", "imes", "ober", "ufat"];
 
@@ -13,9 +14,14 @@ for (let i = 0; i < 2; i++) {
     botoes[i].onclick = () => {
         let texto = textarea.value;
         if (texto) {
-            let resultado = "";
             container_conversao.classList.remove ("d-none");
             container_mensagem.classList.add ("d-none");
+            for (let i = 0; i < acentos.length; i++) {
+                for (let letra_acentuada of acentos[i]) {
+                    let regex = new RegExp (letra_acentuada, "g");
+                    texto = texto.replace (regex, letras[i]);
+                }
+            }
             if (i == 0) {
                 texto = texto.split ("")
                 for (let x = 0; x < texto.length; x++) {
